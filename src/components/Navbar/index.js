4e922517bcf00f8b8c50ee884177226f71da084b
Jhon Castillo Guaita <JhonCastilloG89@hotmail.com>
2021-07-07 17:50:49 -0500

    project done

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react'
+import UserContext from '../../context/UserContext'
+
+const Navbar = () => {
+    const { user, login, logout } = useContext(UserContext);
+
+    return (
+        <nav className="navbar navbar-dark bg-dark mb-4">
+            <div className="container">
+                <span className="navbar-brand">
+                    <h2>{ user ? `Hola ${user.name}` : 'Bienvenid@'}</h2> 
+                </span>
+
+                { user 
+                    ? <button className="btn btn-primary" onClick={logout}>
+                        Cerrar Sesión
+                      </button>
+                    : <button className="btn btn-primary" onClick={login}>
+                        Iniciar Sesión
+                      </button>
+                }
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
